3b38821860290310b69bed5321bae1aa928f3f76
svikas641 <svikas641@gmail.com>
2024-12-29 01:30:42 +0530

    Add /api/hello endpoint

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello, World!" });
+});
+
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
